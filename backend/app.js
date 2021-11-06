1905f0c2632f023cbc72c325b4b71aa632146175
shvartem <shvartem422@yandex.ru>
2021-11-06 23:49:03 +0500

    test

--- backend/app.js
+++ backend/app.js
@@ -40,6 +40,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(requestLogger);
 
+app.use((req, _, next) => {
+  console.log({ user: req.user });
+  next();
+});
 app.use('/signin', loginUserValidation, login);
 app.use('/signup', newUserValidation, createNewUser);
 app.delete('/logout', logout);

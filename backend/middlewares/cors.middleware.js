7385daf5237cfd16d5164bbd574d2e1b3cc1eb32
shvartem <shvartem422@yandex.ru>
2021-11-06 16:26:27 +0500

    test

--- backend/middlewares/cors.middleware.js
+++ backend/middlewares/cors.middleware.js
@@ -2,13 +2,13 @@ const allowedCors = [
   'https://mesto422.nomoredomains.work/',
   'http://mesto422.nomoredomains.work/',
 ];
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,POST,PUT,PATCH,DELETE,OPTIONS';
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,POST,PUT,PATCH,DELETE';
 
 function checkCors(req, res, next) {
   const requestHeaders = req.headers['access-control-request-headers'];
   const { origin } = req.headers;
   const { method } = req;
-
+  console.log({ method, origin, requestHeaders });
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     if (method === 'OPTIONS') {

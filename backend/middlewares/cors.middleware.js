70ae88c95d7c6c61bd1f9f5f5109d7b8d8a6ecf3
shvartem <shvartem422@yandex.ru>
2021-11-06 23:24:13 +0500

    test

--- backend/middlewares/cors.middleware.js
+++ backend/middlewares/cors.middleware.js
@@ -14,6 +14,7 @@ function checkCors(req, res, next) {
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     if (method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Credentials', true);
       res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
       res.header('Access-Control-Allow-Headers', requestHeaders);
 
